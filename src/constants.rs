a482aae4fb2ed94ae92e290ced0fc2412b87b521
tgblackburn <tomblackburn@gmail.com>
2020-05-27 14:15:02 +0200

    Lint fixes

--- src/constants.rs
+++ src/constants.rs
@@ -3,10 +3,12 @@
 /// Speed of light in vacuum, units of m/s
 pub const SPEED_OF_LIGHT: f64 = 2.997925e8;
 /// Speed of light in vacuum, squared, units of m^2/s^2
+#[allow(unused)]
 pub const SPEED_OF_LIGHT_SQD: f64 = 89875517873681764.0;
 /// epsilon_0
 pub const VACUUM_PERMITTIVITY: f64 = 8.854188e-12;
 /// mu_0
+#[allow(unused)]
 pub const VACUUM_PERMEABILITY: f64 = 1.256637e-6;
 /// Units of C
 pub const ELECTRON_CHARGE: f64 = -1.602177e-19;
@@ -25,4 +27,5 @@ pub const ALPHA_FINE: f64 = 7.29735257e-3;
 /// Reduced Compton length / speed of light = hbar / (m c^2)
 pub const COMPTON_TIME: f64 = 1.28808867e-21;
 /// Classical electron radius = alpha * Compton length
+#[allow(unused)]
 pub const CLASSICAL_ELECTRON_RADIUS: f64 = 2.817940e-15;
